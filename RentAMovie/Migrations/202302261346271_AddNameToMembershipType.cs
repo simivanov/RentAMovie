00ec7ded2eaafed3c0aa62906b6b7091b3695633
unknown <simivanov@yahoo.com>
2023-02-28 00:11:31 +0200

    First Commit (movie,customer,genre,membership type models)

---
+++ RentAMovie/Migrations/202302261346271_AddNameToMembershipType.cs
@@ -0,0 +1,20 @@
+namespace RentAMovie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String());
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
